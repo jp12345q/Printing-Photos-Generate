3de98cef285c8c28cf347b32be0cc215d16b0ad1
jp12345q <verdad.194305@meycauayan.sti.edu.ph>
2024-10-06 19:37:44 +0800

    update added animation, layout also some chande on script

--- script/pdfscreen.js
+++ script/pdfscreen.js
@@ -1,5 +1,5 @@
 fetch('form/paperform/previewpdf.html')
     .then(response => response.text())
     .then(data => {
-        document.getElementById('preview').innerHTML = data;
+        document.getElementById('preview1').innerHTML = data;
     });
